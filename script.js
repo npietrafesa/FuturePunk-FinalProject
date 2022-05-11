6ea594e95cdcd917d71bd27733067e5b2c1566b7
npietrafesa <npietrafesa110@gmail.com>
2022-05-11 22:40:28 +0400

    framework for the website down

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.querySelector("body").classList.toggle("active");
+//
+// var bars = document.querySelector(".article-title");
+// bars.addEventListener("click", function(){
+//     document.querySelector("body").classList.toggle("active");
+// })
