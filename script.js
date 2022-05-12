e9cdeff94a77531e1f46746d0f9292aa2943f16b
npietrafesa <npietrafesa110@gmail.com>
2022-05-12 23:39:35 +0400

    finished website

--- script.js
+++
@@ -1,6 +0,0 @@
-// document.querySelector("body").classList.toggle("active");
-//
-// var bars = document.querySelector(".article-title");
-// bars.addEventListener("click", function(){
-//     document.querySelector("body").classList.toggle("active");
-// })
